780517e7b71167ec0355ad3c6bbc7d97af4c5424
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-13 15:11:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,19 @@
 # require modules here
+require 'pry'
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file)
+  file = YAML.load_file('lib/emoticons.yml')
+  library = Hash.new
+  file.each do |key, value|
+    library[key] = {}
+    library[key][:english] = value[0]
+    library[key][:japanese] = value[1]
+  end
+  library
 end
+
+
 
 def get_japanese_emoticon
   # code goes here
