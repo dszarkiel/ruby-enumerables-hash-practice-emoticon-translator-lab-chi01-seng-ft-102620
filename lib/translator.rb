b41657738da6140e1276f199f23ca70fd9a8704f
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-13 16:34:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,14 +13,18 @@ def load_library(file)
   library
 end
 
-def get_english_meaning(file, japemoji)
+def get_english_meaning(file, jap_emoji)
   hoh = load_library(file)
-  japemoji = hoh.keys.find do |key|
-    hoh[key][:japanese] == japemoji
+  jap_emoji = hoh.keys.find do |key|
+    hoh[key][:jap_anese] == jap_emoji
   end
-  japemoji ? japemoji : "Sorry, that emoticon was not found"
+  jape_moji ? jap_emoji : "Sorry, that emoticon was not found"
 end
 
-#def get_japanese_emoticon
-  # code goes here
-#end
+def get_japanese_emoticon(file, eng_emoji)
+  hoh = load_library(file)
+  eng_emoji = hoh.keys.find do |key|
+    hoh[key][:english] == eng_emoji
+end
+eng_emoji ? eng_emoji : "Sorry, that emoticon was not found"
+end
